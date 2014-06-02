ae9c5a60bd6fb758a0d20c2073c75319a832d240
Hannah Headrick <hannah@netengine.com.au>
2014-06-02 15:07:54 +1000

    redhat forum progress

---
+++ src/templates/forum-website/js/jquery.lettering.js
@@ -0,0 +1,77 @@
+/*global jQuery */
+/*!	
+* Lettering.JS 0.6.1
+*
+* Copyright 2010, Dave Rupert http://daverupert.com
+* Released under the WTFPL license 
+* http://sam.zoy.org/wtfpl/
+*
+* Thanks to Paul Irish - http://paulirish.com - for the feedback.
+*
+* Date: Mon Sep 20 17:14:00 2010 -0600
+*/
+(function($){
+	function injector(t, splitter, klass, after) {
+		var a = t.text().split(splitter), inject = '';
+		var n_letter = a.length;
+		if (n_letter) {
+			$(a).each(function(i, item) {
+				item = item.toLowerCase();
+				//inject += '<span class="'+klass+(i+1)+'">'+item+'</span>'+after;
+				inject += '<span class="'+item+' ir">'+item+'</span>'+after;
+			});
+			if (!t.parent().hasClass('city')) {
+				if (n_letter<9) {
+					var n_blank = 9-n_letter;
+					for (var i=0;i<n_blank;i++) {
+						inject += '<span></span>';
+					}
+				}
+			}
+			t.empty().append(inject);
+		}
+	}
+	
+	var methods = {
+		init : function() {
+
+			return this.each(function() {
+				injector($(this), '', 'char', '');
+			});
+
+		},
+
+		words : function() {
+
+			return this.each(function() {
+				injector($(this), ' ', 'word', ' ');
+			});
+
+		},
+		
+		lines : function() {
+
+			return this.each(function() {
+				var r = "eefec303079ad17405c889e092e105b0";
+				// Because it's hard to split a <br/> tag consistently across browsers,
+				// (*ahem* IE *ahem*), we replace all <br/> instances with an md5 hash 
+				// (of the word "split").  If you're trying to use this plugin on that 
+				// md5 hash string, it will fail because you're being ridiculous.
+				injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
+			});
+
+		}
+	};
+
+	$.fn.lettering = function( method ) {
+		// Method calling logic
+		if ( method && methods[method] ) {
+			return methods[ method ].apply( this, [].slice.call( arguments, 1 ));
+		} else if ( method === 'letters' || ! method ) {
+			return methods.init.apply( this, [].slice.call( arguments, 0 ) ); // always pass an array
+		}
+		$.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
+		return this;
+	};
+
+})(jQuery);
