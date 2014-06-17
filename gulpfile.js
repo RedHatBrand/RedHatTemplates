ccb3a71ba049d6705f14b8c2d5e6996e68adb5b4
Hannah Headrick <hannah@netengine.com.au>
2014-06-17 17:02:31 +1000

    RedHat agenda and registration board

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,7 @@ handlebars.registerHelper('json', function(context) {
 function url(){
   return function addUrl(files, metalsmith, done){
     for (var file in files) {
-      files[file].url = file;
+      files[file].url = '/' + file;
     }
     done();
   };
