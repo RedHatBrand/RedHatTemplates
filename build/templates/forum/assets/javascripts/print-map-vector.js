ca2152c1dc8ab997d9b7b834ae2fdc75f8dafe13
Brad Parker <hi@bradparker.com>
2014-06-24 16:02:52 +1000

    Need to remove load on client
    No roads on vector maps for right now

--- build/templates/forum/assets/javascripts/print-map-vector.js
+++ build/templates/forum/assets/javascripts/print-map-vector.js
@@ -32,21 +32,6 @@ function initMap (events, selectedLocation) {
   })
   water.addTo(map);
 
-  // Highways from OpenStreetMap
-  var roadSizes = {
-    "highway": "2px",
-    "major_road": "1px",
-    "minor_road": "0.5px",
-    "rail": "0.125px",
-    "path": "0.25px"
-  };
-  var roads = new L.TileLayer.d3_topoJSON("http://tile.openstreetmap.us/vectiles-highroad/{z}/{x}/{y}.topojson", {
-    class: "road",
-    layerName: "vectile",
-    style: function(d) { return "stroke-width: " + roadSizes[d.properties.kind]; }
-  })
-  roads.addTo(map);
-
   var markersList = map.markersList = {};
   var markers = L.featureGroup();
 
